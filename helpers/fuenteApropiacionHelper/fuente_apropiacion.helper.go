770966668013cdf265377c8bf4f72d1578116c93
Juan Camilo Sarmiento Reyes <jcamilosarmientor@gmail.com>
2019-07-06 21:57:01 -0500

    Servicio para registro multiple de movimientos

--- helpers/fuenteApropiacionHelper/fuente_apropiacion.helper.go
+++ helpers/fuenteApropiacionHelper/fuente_apropiacion.helper.go
@@ -9,14 +9,17 @@ import (
 	"github.com/udistrital/utils_oas/request"
 )
 
-// URLCRUD Ruta de plan_cuentas_crud
+// URLCRUD Path de plan_cuentas_crud
 var URLCRUD = beego.AppConfig.String("planCuentasApiService") + "fuente_financiamiento_apropiacion/registrar_multiple"
+
+// URLMOVIMIENTOSCRUD Path de movimientos_crud
+var URLMOVIMIENTOSCRUD = beego.AppConfig.String("movimientosCrudService") + "movimiento_proceso_externo/registrar_multiple"
 
 // RegistrarMultipleFuenteApropiacion utiliza la transacción de fuente_financiamiento_apropiacion/registrar_multiple
 // para registrar multiples datos en fuente_financiamiento_apropiacion
 func RegistrarMultipleFuenteApropiacion(fuentesApropiacion []*models.FuenteFinanciamientoApropiacion) (idRegistrados []int) {
 	var (
-		res          map[string]interface{}
+		res          map[string]interface{} // Respuesta de una petición
 		bodyResponse map[string][]int
 	)
 
@@ -30,6 +33,30 @@ func RegistrarMultipleFuenteApropiacion(fuentesApropiacion []*models.FuenteFinan
 		return
 	}
 	idRegistrados = bodyResponse["Ids"]
+	return
+}
+
+// RegistrarMultipleMovimientoExterno utiliza la transacción registrar_multiple de movimientos_crud
+// para registrar multiples movimientos en una sola petición
+// @param el parámetro movimientos es un arreglo con la información de todos los movimientos a registar
+func RegistrarMultipleMovimientoExterno(movimientos []interface) (idRegisrados []int) {	
+	// var (
+	// 	res map[string]interface{} // Respuesta de una petición
+	// 	bodyResponse map[string][int] // Cuerpo de respuesta de la petición
+	// )
+
+	// data := movimientos
+
+	// if err := request.SendJson(URLMOVIMIENTOSCRUD, "POST", &res, &movimientos); err != nil {
+	// 	log.Panicln(err.Error())
+	// 	return
+	// }
+
+	// if err := formatdata.FillStruct(res["Body"], &bodyResponse); err != nil {
+	// 	log.Panicln(err.Error())
+	// 	return
+	// }
+	// idRegistrados = bodyResponse["Ids"]
 	return
 }
 
