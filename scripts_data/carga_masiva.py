c64ed7b9cd569d0d86ad0f4da782a0b25fc18601
macaguegi <mcguerrerog@correo.udistrital.edu.co>
2019-07-09 14:24:26 -0500

    modelo rubro añadido #19

--- scripts_data/carga_masiva.py
+++ scripts_data/carga_masiva.py
@@ -1,5 +1,6 @@
 import csv
 import requests 
+import json
   
 
 # Se abre el archivo CSV u se imprimen los registros del mismo.
@@ -13,14 +14,25 @@ csvFile.close()
 # Procesamiento para dar formato al árbol
 
 # Diccionario de datos con la estructura del arbol de rubros que será enviada para el POST
-data = {'CODIGO' : CODIGO,
-        'NOMBRE' : NOMBRE,
-        'ALGO' : 'ALGO'
+data = {'Id' : 1,
+        'Organizacion' : 1,
+        'Codigo' : '1',
+        'Descripcion' : 'AA',
+        'UnidadEjecutora' : 1,
+        'Nombre' : 'AA'
         }
 
 # Endpoints
-PLAN_CUENTAS_CRUD = "http://maps.googleapis.com/maps/api/geocode/json"
-PLAN_CUENTAS_MONGO_CRUD = "http://maps.googleapis.com/maps/api/geocode/json"
+PLAN_CUENTAS_CRUD = "https://"
+PLAN_CUENTAS_MONGO_CRUD = "https://"
 
-r_crud = requests.post(url = PLAN_CUENTAS_CRUD, data = data) 
+r_crud = requests.post(url = PLAN_CUENTAS_CRUD, data = json.dumps(data)) 
 r_mongo_crud = requests.post(url = PLAN_CUENTAS_MONGO_CRUD, data = data)
+
+# JSON Respuesta
+response_crud = r_crud.text
+response_mongo_crud = r_mongo_crud.text
+
+# Status response
+status_crud = r_crud.status_code
+status_mongo_crud = r_mongo_crud.status_code
