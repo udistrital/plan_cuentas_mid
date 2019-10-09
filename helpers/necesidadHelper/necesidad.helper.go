7ed59691df6cd7613e48856e470eab6b95cc354a
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2019-10-09 17:21:25 -0500

    avance init necesidad #30

--- helpers/necesidadHelper/necesidad.helper.go
+++ helpers/necesidadHelper/necesidad.helper.go
@@ -2,29 +2,47 @@ package necesidadhelper
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/udistrital/plan_cuentas_mid/models"
+	"github.com/udistrital/utils_oas/formatdata"
 	"github.com/udistrital/utils_oas/request"
 )
 
 // InitNecesidad init necesidad
-func InitNecesidad(id string) (necesidad map[string]interface{}, outputError map[string]interface{}) {
+func InitNecesidad(id string) (necesidad map[string]interface{}, err map[string]interface{}) {
 	var (
-		necesidadPC  map[string][]interface{}
-		necesidadADM map[string][]interface{}
+		necesidadPC map[string][]interface{}
 	)
 	necesidad = make(map[string]interface{})
 	necesidad["id"] = id
 	urlmongo := beego.AppConfig.String("financieraMongoCurdApiService") + "necesidades/?query=idAdministrativa:" + id
-	urladm := beego.AppConfig.String("adminCrudService") + "necesidad/?query=Id:" + id
-	if err := request.GetJson(urladm, &necesidadADM); err != nil {
-		panic("ADM API Service Error")
+	if necesidadADM, err := GetNecesidadADM(id); err != nil {
+		panic(err)
 	} else {
-		beego.Info(necesidadADM)
+		necesidad["necesidadADM"] = necesidadADM
 	}
 	if err := request.GetJson(urlmongo, &necesidadPC); err != nil {
-		panic("Mongo API Service Error")
+		panic(err)
 	} else {
 		necesidad["NecesidadPC"] = necesidadPC["Body"][0]
-		necesidad["NecesidadADM"] = necesidadADM
 		return necesidad, nil
+	}
+}
+
+// GetNecesidadADM obtiene necesidad de adm crud apí
+func GetNecesidadADM(id string) (necesidad map[string]interface{}, outErr map[string]interface{}) {
+	var necesidadADM []map[string]interface{}
+	var necesidadAux models.NecesidadADM
+	urladm := beego.AppConfig.String("adminCrudService") + "necesidad/?query=Id:" + id
+	if err := request.GetJson(urladm, &necesidadADM); err != nil {
+		outErr = map[string]interface{}{"Function": "GetNecesidadADM", "Error": err.Error()}
+		return nil, outErr
+	} else {
+		if err := formatdata.FillStruct(necesidadADM[0], &necesidadAux); err == nil {
+			necesidad, err = formatdata.ToMap(necesidadAux, "json")
+			return necesidad, nil
+		} else {
+			outErr = map[string]interface{}{"Function": "GetNecesidadADM", "Error": err.Error()}
+			return nil, outErr
+		}
 	}
 }
