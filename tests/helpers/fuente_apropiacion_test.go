8b1af9b2475b7eea72341092c8b0926d52c729e7
NeiderFajardo <neafajardoc@correo.udistrital.edu.co>
2020-01-19 21:58:41 -0500

    segunda prueba

--- tests/helpers/fuente_apropiacion_test.go
+++ tests/helpers/fuente_apropiacion_test.go
@@ -2,7 +2,10 @@ package test
 
 import (
 	fuenteapropiacionhelper "github.com/udistrital/plan_cuentas_mid/helpers/fuenteApropiacionHelper"
+	"github.com/udistrital/plan_cuentas_mid/models"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestAddTotal(t *testing.T) {
@@ -30,4 +33,57 @@ func TestAddTotal(t *testing.T) {
 		t.Errorf("AddTotal Fail, expected 160, got %v", result)
 		t.Fail()
 	}
+}
+
+func TestConvertModificacionToDocumentoPresupuestal(t *testing.T) {
+	var testData models.ModificacionFuenteReceiver
+	var dataObj models.ModificacionPresupuestalReceiverDetail
+	var testTipoGeneral models.TipoGeneral
+	var testRubro models.Rubro
+	var testFuenteFinanciamiento models.FuenteFinanciamiento
+	var testTipoFuenteFinanciamiento models.TipoFuenteFinanciamiento
+	var testModificacionReciever models.ModificacionFuenteReceiverAfectation
+
+	testTipoGeneral.Id = 1
+	testTipoGeneral.Nombre = "test"
+	testTipoGeneral.Descripcion = "test"
+	testTipoGeneral.Acronimo = "test"
+	testTipoGeneral.Parametros = "{\"test\" : true }"
+
+	dataObj.DocumentNumber = "test"
+	dataObj.DocumentDate = time.Now()
+	dataObj.Descripcion = "test"
+	dataObj.CentroGestor = "test"
+	dataObj.OrganismoEmisor = "test"
+	dataObj.DocumentType = &testTipoGeneral
+
+	testData.Data = &dataObj
+
+	testRubro.Id = 1
+	testRubro.Organizacion = 1
+	testRubro.Codigo = "test"
+	testRubro.Descripcion = "test"
+	testRubro.UnidadEjecutora = "test"
+	testRubro.Nombre = "test"
+
+	testTipoFuenteFinanciamiento.Id = 1
+	testTipoFuenteFinanciamiento.Nombre = "test"
+	testTipoFuenteFinanciamiento.Descripcion = "test"
+
+	testFuenteFinanciamiento.Id = 1
+	testFuenteFinanciamiento.Nombre = "test"
+	testFuenteFinanciamiento.Descripcion = "test"
+	testFuenteFinanciamiento.Codigo = "test"
+	testFuenteFinanciamiento.TipoFuenteFinanciamiento = &testTipoFuenteFinanciamiento
+
+	testModificacionReciever.OriginAcc = &testFuenteFinanciamiento
+	testModificacionReciever.OriginRubro = &testRubro
+	testModificacionReciever.TypeMod = &testTipoGeneral
+	testModificacionReciever.Amount = 1
+	testData.Afectation = append(testData.Afectation, &testModificacionReciever,
+		&testModificacionReciever)
+
+	testOutput := fuenteapropiacionhelper.ConvertModificacionToDocumentoPresupuestal(testData)
+	t.Log(reflect.TypeOf(testOutput))
+	//if reflect.TypeOf(testData) == ""
 }
