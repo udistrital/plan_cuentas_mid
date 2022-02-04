7f126227d3d8253e1104f2cb28c4e65d0be68d46
brayanpasa99 <brayanpasa99@hotmail.com>
2022-02-04 13:09:13 -0500

    refactor: Ajuste tipos de dato para aprobacion
    Resuelve: udistrital/presupuesto_cliente#469

--- controllers/cdp.go
+++ controllers/cdp.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/astaxie/beego"
 	cdphelper "github.com/udistrital/plan_cuentas_mid/helpers/cdpHelper"
@@ -69,7 +70,7 @@ func (c *CdpController) SolicitarCdp() {
 // @Failure 403 body is empty
 // @router /aprobar_cdp [post]
 func (c *CdpController) AprobarCdp() {
-	var v map[string]string
+	var v map[string]interface{}
 
 	defer func() {
 		if r := recover(); r != nil {
@@ -78,11 +79,26 @@ func (c *CdpController) AprobarCdp() {
 		}
 	}()
 
+	// logs.Debug("c.Ctx.Input.RequestBody: ", c.Ctx.Input.RequestBody)
+
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err != nil {
 		responseformat.SetResponseFormat(&c.Controller, err, "E_0458", 500)
 	}
 
-	if response, err := cdphelper.AprobarCdp(v["_id"], v["vigencia"], v["area_funcional"]); err == nil {
+	// logs.Debug("v: ", v)
+
+	for i, value := range v {
+		switch value.(type) {
+		case float64:
+			v[i] = fmt.Sprintf("%.0f", value)
+			// logs.Debug("v[i]: ", v[i])
+			// logs.Debug("reflect.TypeOf(v[i]): ", reflect.TypeOf(v[i]))
+		default:
+			// logs.Debug("reflect.TypeOf(value)", reflect.TypeOf(value))
+		}
+	}
+
+	if response, err := cdphelper.AprobarCdp(v["_id"].(string), v["vigencia"].(string), v["area_funcional"].(string)); err == nil {
 		responseformat.SetResponseFormat(&c.Controller, response, "", 200)
 	} else {
 		responseformat.SetResponseFormat(&c.Controller, err, "E_0458", 500)
